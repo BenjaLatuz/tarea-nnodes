a47b5857a47e1f02d21788aaac9c7095073323b0
Benja <benja@LAPTOP-QTHLGBGR.>
2025-05-22 23:33:58 -0400

    Agregada funcionalidad en Modelo Round para elegir un resultado según porcentajes automáticamente

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,9 +4,21 @@ class Round < ApplicationRecord
   validates :result, presence: true, inclusion: { in: COLORS }
   validates :played_at, presence: true
 
+  before_validation :set_result
   before_validation :set_played_at, on: :create
 
   private
+
+  def set_result
+    random = rand(100)
+    self.result = if random < 2
+                    'verde'
+                  elsif random < 51
+                    'rojo'
+                  else
+                    'negro'
+                  end
+  end
 
   def set_played_at
     self.played_at ||= Time.current
