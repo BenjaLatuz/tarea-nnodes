9503617419727255e3def084f29e17efabc87524
Benja <benja@LAPTOP-QTHLGBGR.>
2025-05-23 15:43:05 -0400

    Reorganiza la navegación y botones: Agrega enlace a Jugadores en el navbar, botón crear jugador, vista de todos los jugadores

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,7 +25,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: "Player was successfully created." }
+        format.html { redirect_to root_path, notice: "Jugador creado exitosamente" }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new, status: :unprocessable_entity }
